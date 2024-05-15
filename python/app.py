69c739fa537a887bc27381fd1909a80981e8e9fa
marvin thomas <91838044+marvin2255@users.noreply.github.com>
2024-05-15 15:27:26 +0530

    Update app.py

--- python/app.py
+++ python/app.py
@@ -1,1 +1,6 @@
-print("hello world")
+def this_trail( int x, int y):
+    sum=x+y
+   return sum
+x=input(int ("enter the first   numbers"))
+y=intput(int("enter the second number "))
+this_trail=(x,y)
